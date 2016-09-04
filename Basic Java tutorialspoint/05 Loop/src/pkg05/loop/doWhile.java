38a672c237a18e126cd59a229868431372df0758
Mamun Sarkar <mamun.atova@gmail.com>
2016-09-04 13:59:25 +0600

    Added loop, conditions

---
+++ Basic Java tutorialspoint/05 Loop/src/pkg05/loop/doWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg05.loop;
+
+/**
+ *
+ * @author mamun
+ */
+public class doWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("How " + i);
+            i++;
+        } while (i <= 10);
+    }
+}
---
+++ Basic Java tutorialspoint/05 Loop/src/pkg05/loop/whileLoop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg05.loop;
+
+/**
+ *
+ * @author mamun
+ */
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {            
+            System.out.println("How " + i);
+            i++;
+        }
+    }
+}
